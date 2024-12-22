fe0efcde2e83e99966a6de1497236260cc97d1c5
Erfan-aminian <aminian158@gmail.com>
2024-12-22 22:48:55 +0330

    add

--- Rest/home/views.py
+++ Rest/home/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from django.http import HttpResponse
 # Create your views here.
+class HomeView(View):
+    def get(self, request):
+        return HttpResponse('hello world!')
+
