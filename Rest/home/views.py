b1e423b8e6bdc1c90b17f4ac87460c8d1b20134f
Erfan-aminian <aminian158@gmail.com>
2025-01-01 20:47:50 +0330

    update

--- Rest/home/views.py
+++ Rest/home/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.http import HttpResponse
-# Create your views here.
-class HomeView(View):
-    def get(self, request):
-        return HttpResponse('hello world!')
+#rest_freamework
+from rest_framework.decorators import api_view # for function base view
+from rest_framework.response import Response
+from rest_framework.views import APIView # for cbv
 
+# Create your views here.
+
+'''function base views code'''
+#@api_view(['GET', 'POST', 'PUT'])
+#def Home (request):
+    #return Response({'name':'amir'})
+
+''' Class base View '''
+class Home(APIView):
+    def get(self, request):
+        return Response({'name': 'jack'})
