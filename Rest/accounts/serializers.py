2ea60139484d9857f6cb10a9be3013b2c1edad3a
Erfan-aminian <aminian158@gmail.com>
2025-01-07 23:19:02 +0330

    Model serializers

--- Rest/accounts/serializers.py
+++ Rest/accounts/serializers.py
@@ -1,15 +1,38 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+
 
 def clean_email(value):
     if 'admin' in value:
         raise serializers.ValidationError('admin cant be in email')
 
 
-class UserRegisterSerializers(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    email = serializers.EmailField(required=True, validators=[clean_email])
-    password = serializers.CharField(required=True, write_only=True)
-    password2 = serializers.CharField(required=True, write_only=True)
+# class UserRegisterSerializers(serializers.Serializer):
+#     username = serializers.CharField(required=True)
+#     email = serializers.EmailField(required=True, validators=[clean_email])
+#     password = serializers.CharField(required=True, write_only=True)
+#     password2 = serializers.CharField(required=True, write_only=True)
+
+    # def validate_username(self, value):
+    #     if value == 'admin':
+    #         raise serializers.ValidationError('username cant be admin')
+    #     return value
+    #
+    # def validate(self, data):
+    #     if data['password'] != data['password2']:
+    #         raise serializers.ValidationError('password must match')
+    #     return data
+
+class UserRegisterSerializers(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'email': {'validators': (clean_email,)}
+        }
 
     def validate_username(self, value):
         if value == 'admin':
@@ -20,4 +43,3 @@ class UserRegisterSerializers(serializers.Serializer):
         if data['password'] != data['password2']:
             raise serializers.ValidationError('password must match')
         return data
-
