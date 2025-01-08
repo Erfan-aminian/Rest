4cbdb182442e4fd1101b296286c20faa10ed4f69
Erfan-aminian <aminian158@gmail.com>
2025-01-08 21:08:02 +0330

    Status Code

--- Rest/accounts/views.py
+++ Rest/accounts/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import UserRegisterSerializers
-
+from rest_framework import status
 
 # Create your views here.
 class UserRegister(APIView):
@@ -11,5 +11,5 @@ class UserRegister(APIView):
         ser_data = UserRegisterSerializers(data=request.POST)
         if ser_data.is_valid():
             ser_data.create(ser_data.validated_data)
-            return Response(ser_data.data)
-        return Response(ser_data.errors)
+            return Response(ser_data.data, status=status.HTTP_201_CREATED)
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
