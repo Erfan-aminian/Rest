ff89e8cdd0c2bb836649f9eb441293c8a4181e95
Erfan-aminian <aminian158@gmail.com>
2025-01-08 20:59:37 +0330

    Override create

--- Rest/accounts/serializers.py
+++ Rest/accounts/serializers.py
@@ -34,6 +34,10 @@ class UserRegisterSerializers(serializers.ModelSerializer):
             'email': {'validators': (clean_email,)}
         }
 
+    def create(self, validated_data):
+        del validated_data['password2']
+        return User.objects.create_user(**validated_data)
+
     def validate_username(self, value):
         if value == 'admin':
             raise serializers.ValidationError('username cant be admin')
