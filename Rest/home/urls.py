a7fc60092c8f58ce113bb253eda3f818d1808884
Erfan-aminian <aminian158@gmail.com>
2025-01-12 22:20:09 +0330

    write add model,update and delete

--- Rest/home/urls.py
+++ Rest/home/urls.py
@@ -5,5 +5,6 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('question/', views.QuestionView.as_view())
+    path('question/', views.QuestionView.as_view()),
+    path('question/<int:pk>/', views.QuestionView.as_view()),
 ]
--- Rest/home/views.py
+++ Rest/home/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 #rest_freamework
 from rest_framework.decorators import api_view # for function base view
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView # for cbv
 from .models import Person
 from .serializers import PersonSerializer, QuestionSerializer, AnswerSerializer
@@ -38,5 +38,25 @@ class QuestionView(APIView):
     def get(self, request):
         question = Question.objects.all()
         ser_data = QuestionSerializer(instance=question, many=True)
-        return Response(ser_data.data, status=HTTP_200_OK)
+        return Response(ser_data.data, status=status.HTTP_200_OK)
+
+    def post(self,request):
+        ser_data = QuestionSerializer(data=request.POST)
+        if ser_data.is_valid():
+            ser_data.save()
+            return Response(ser_data.data, status=status.HTTP_201_CREATED)
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        question = Question.objects.get(pk=pk)
+        ser_data = QuestionSerializer(instance=question, data=request.data, partial=True)
+        if ser_data.is_valid():
+            ser_data.save()
+            return Response(ser_data.data, status=status.HTTP_200_OK)
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete (self, request, pk):
+        question = Question.objects.get(pk=pk)
+        question.delete()
+        return Response({'message':'question deleted'})
 
