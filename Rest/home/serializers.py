a1e6b51497532b5b35e338a358561f04e6d15a4f
Erfan-aminian <aminian158@gmail.com>
2025-01-14 22:40:22 +0330

    add custom relational fields

--- Rest/home/serializers.py
+++ Rest/home/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Question, Answer
+from .custom_relational_fields import UserEmailNameRelationalField
+
 
 class PersonSerializer(serializers.Serializer):
     name = serializers.CharField()
@@ -8,6 +10,8 @@ class PersonSerializer(serializers.Serializer):
 
 class QuestionSerializer(serializers.ModelSerializer):
     answers = serializers.SerializerMethodField()
+    user = UserEmailNameRelationalField(read_only=True)
+
     class Meta:
         model = Question
         fields = '__all__'
