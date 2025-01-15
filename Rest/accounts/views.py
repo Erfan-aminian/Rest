b1a81493782694d3d5a4c4aa4f94b2cd58dd6b16
Erfan-aminian <aminian158@gmail.com>
2025-01-15 11:17:26 +0330

    Add ViewSet

--- Rest/accounts/views.py
+++ Rest/accounts/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import UserRegisterSerializers
 from rest_framework import status
-
+from rest_framework import viewsets
 # Create your views here.
 class UserRegister(APIView):
     def post(self, request):
@@ -13,3 +13,17 @@ class UserRegister(APIView):
             ser_data.create(ser_data.validated_data)
             return Response(ser_data.data, status=status.HTTP_201_CREATED)
         return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserViewSer(viewsets.ViewSet):
+    def list(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
